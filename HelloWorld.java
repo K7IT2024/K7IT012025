6a35750a616f237b37c71b6f59cfb1dbab672a60
K7IT2024 <dhanyavani1991@gmail.com>
2025-11-25 12:27:40 +0530

    updated Helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,10 @@
  class HelloWorld{
 
  public static void main(String args[]){
-    
     System.out.println("Hello World this is my 1st java program from K7it");
     System.out.println("This is first commit to my Git repo");
+
+    System.out.println("This is first commit to my Git Desktop repo");
 
   }
 
